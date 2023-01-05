f41fbd3e25975033946b1719483e72a84add99bb
Christina <cligare@hotmail.com>
2023-01-05 13:30:01 -0800

    created my own digital clock

--- README.md
+++ README.md
@@ -1,21 +1,15 @@
-# coc0840c8a4fe0388a94a4f89
+# My Own Project -- JavaScript Digital Clock
 
-Quick start:
+### The challenge
 
-```
-$ npm install
-$ npm start
-````
+✨✨✨Build a JavaScript Digital Clock from scratch
 
-Head over to https://vitejs.dev/ to learn more about using vite
-## About Scrimba
+### Screenshot
 
-At Scrimba our goal is to create the best possible coding school at the cost of a gym membership! 💜
-If we succeed with this, it will give anyone who wants to become a software developer a realistic shot at succeeding, regardless of where they live and the size of their wallets 🎉
-The Frontend Developer Career Path aims to teach you everything you need to become a Junior Developer, or you could take a deep-dive with one of our advanced courses 🚀
+![](./images/screenshot.png)
 
-- [Our courses](https://scrimba.com/allcourses)
-- [The Frontend Career Path](https://scrimba.com/learn/frontend)
-- [Become a Scrimba Pro member](https://scrimba.com/pricing)
+### Built with
 
-Happy Coding!
+- Semantic HTML5 markup
+- CSS custom properties
+- JavaScript
--- index.js
+++ index.js
@@ -1,52 +1,17 @@
-import { menuArray } from "./data.js";
+// ✅Checklist
 
-// Checklist
-// ✅show menu items
-// add event listener to add items to cart
-// add items to cart
-// render all the items
-// get the total price
-// option to remove items
-// hide order if no items present
-// hide and show modal
-// if no items hide pre-checkout
-// add event listener to submit button
-// ✅ create modal
-// render modal when submit button is clicked
-// make all fields required in modal state
-// when pay button clicked: close modal, create thank you message with user name, return to default state
+// ✅create const of hours and minutes
+// ✅fetch time id and set the current hour and minutes
+// ✅fetch am/pm id and set the correct am/pm
 
-// DECLARE VARIABLES
-const paymentModal = document.getElementById("payment-modal");
-let cartArray = [];
+let d = new Date();
 
-// RENDERING DEFAULT STATE
-function render() {
-  document.getElementById("menu-section").innerHTML = getMenuHtml();
-}
-render();
+let hrs = d.getHours();
+let min = d.getMinutes();
 
-// DEFAULT STATE
-// function to get items from data.js and render as HTML
-function getMenuHtml() {
-  let menuHtml = "";
+document.getElementById("time").textContent =
+  hrs + ":" + (min < 10 ? "0" : "" + min);
 
-  menuArray.forEach(function (item) {
-    menuHtml += `
-      <div class="menu-item">
-        <div class="menu-item-details">
-          <span class="menu-item-emoji">${item.emoji}</span>
-          <div>
-            <h2 class="menu-item-name">${item.name}</h2>
-            <p class="menu-item-ingredients">${item.ingredients.join(", ")}</p>
-            <p class="menu-item-price">$${item.price}</p>
-          </div>  
-        </div>
-        <button class="menu-add-item-btn">+</button>
-      </div>
-    `;
-  });
-  return menuHtml;
-}
+let amPM = document.getElementById("amPM");
 
-// ADD TO CART - PRE-CHECKOUT-STATE
+hrs >= 12 ? (amPM.textContent = "PM") : (amPM.textContent = "AM");
